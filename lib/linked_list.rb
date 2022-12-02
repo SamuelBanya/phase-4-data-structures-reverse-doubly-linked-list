d37b82591729b54f1827f6346723bf5e3b84e794
Samuel Banya <sbanya@fastmail.com>
2022-12-02 15:48:58 -0600

    commit tree ae456033f18ce2b08855be603916ad13d04ebe7b

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,14 +1,31 @@
 require_relative './node'
 
 class LinkedList
-  attr_accessor :head, :tail
+  attr_accessor :head, :tail, :length
 
   def initialize
     @head = nil
     @tail = nil
+    @length = 0
   end
 
-  def reverse!
-    # your code here
+  def reverse(head)
+    # Write a method LinkedList#reverse! that reverses the list in place, 
+    # so the head is the new tail, 
+    # the tail is the new head, 
+    # and all of the prev_node and next_node attributes of each node in the list point to the correct nodes.
+
+    return head if head.length < 2
+
+    current = head
+    new_head = nil
+
+    while(current != nil)
+      new_head = current.prev_node
+      current.prev_node = current.next_node
+      current.next_node = new_head
+      current = current.prev_node
+    end
+    new_head.prev_node
   end
 end
